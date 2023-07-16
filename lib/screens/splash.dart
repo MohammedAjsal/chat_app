c0678bf8bb6d622a6342ae33bd78707a91f889b3
MohammedAjsal <ajsalshaah786@gmail.com>
2023-07-16 20:14:29 +0530

    user authentication added using firebase sdk

---
+++ lib/screens/splash.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('Beyond boundaries..'),
+      // ),
+      body: Center(
+        child: Text('Loading...'),
+      ),
+    );
+  }
+}
